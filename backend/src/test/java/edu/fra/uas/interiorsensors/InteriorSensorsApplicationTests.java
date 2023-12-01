e06f85a9c8d5a5043f5c46d93f39f7411f8c5dae
Mohammed Dawoud <damohammed58@gmail.com>
2023-12-01 11:29:13 +0100

    create new Springboot Project

---
+++ backend/src/test/java/edu/fra/uas/interiorsensors/InteriorSensorsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.fra.uas.interiorsensors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InteriorSensorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
