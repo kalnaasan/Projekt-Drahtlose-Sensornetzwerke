f849605a68d10ba5b97c5c1d3854c2a8bad9a25d
Mohammed Dawoud <damohammed58@gmail.com>
2023-12-05 11:41:50 +0100

    init Frontend

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'interior-sensors' title`, () => {
+  it(`should have the 'interior-senior' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('interior-sensors');
+    expect(app.title).toEqual('interior-senior');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, interior-sensors');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, interior-senior');
   });
 });
