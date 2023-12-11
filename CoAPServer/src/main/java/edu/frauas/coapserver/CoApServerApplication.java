0ed05fbf45b1137d86fdcd49771e491c6f4f1ad3
Kaddour Alnaasan <kaddour.alnaasan@bellavista-film.com>
2023-12-11 22:53:13 +0100

    init CoAPServer

---
+++ CoAPServer/src/main/java/edu/frauas/coapserver/CoApServerApplication.java
@@ -0,0 +1,13 @@
+package edu.frauas.coapserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoApServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoApServerApplication.class, args);
+	}
+
+}
