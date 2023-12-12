497db3494c5c85b1de81c16a6d4061e650c94598
Saam Niko <saamniko@web.de>
2023-12-12 01:31:45 +0100

    sensor functionality outside main, creating json for transmitting via coap

--- sensorConnect/src/main.c
+++ sensorConnect/src/main.c
@@ -1,102 +1,21 @@
 #include <zephyr/net/openthread.h>
 #include <openthread/thread.h>
 #include <openthread/coap.h>
+//#include <zephyr/data/json.h>
+#include <stdio.h>
 
-//#include "sensor_functionality.h"
-#include "scd4x_i2c.h"
-#include "svm41_i2c.h"
-#include "sensirion_common.h"
-#include "sensirion_i2c_hal.h"
+#include "sensor_functionality.h"
 
-typedef enum{
-    SLEEP,
-	SENSOR_INIT,
-	PERIODIC_MEASURING,   
-} sensor_state;
+#define SLEEP_TIME_MS 1000
+#define TEXTBUFFER_SIZE 256
 
-static sensor_state s_state = SLEEP;
+/* CoAP Module */
+static void coap_send_data_request(void);
+static void coap_send_data_response_cb(void * p_context, otMessage * p_message,
+										const otMessageInfo * p_message_info, otError result);
+void coap_init(void);
 
-/* for now, data is stored like this, will be omitted later*/
-static uint16_t SC41_co2;
-static int32_t SCD41_temperature;
-static int32_t SCD41_humidity;
-
-static int16_t SVM41_humidity;
-static int16_t SVM41_temperature;
-static int16_t SVM41_voc_index;
-static int16_t SVM41_nox_index;
-
-
-static bool valid_SC41_data = false;
-static bool valid_SVM41_data = false;
-
-
-/* Multithreading */
-#define THREAD0_STACKSIZE 512
-#define THREAD1_STACKSIZE 512
-#define THREAD0_PRIORITY 4 
-#define THREAD1_PRIORITY 4
-K_MUTEX_DEFINE(test_mutex);
-
-
-void shared_code_section(void){
-	/* Lock the mutex */
-	k_mutex_lock(&test_mutex, K_FOREVER);
-	/* Do something */
-	/* Unlock the mutex */
-	k_mutex_unlock(&test_mutex);
-}
-
-/* Sensor functionality */
-void clean_up_sensor_states(int16_t* error) {
-	// SCD41
-	scd4x_wake_up();
-	scd4x_stop_periodic_measurement();
-    error = scd4x_reinit();
-    if (error) {
-        printk("Error executing scd4x_reinit(): %i\n", error);
-    }
-	// SVM41
-	error = svm41_device_reset();
-    if (error) {
-        printk("Error executing svm41_device_reset(): %i\n", error);
-    }
-}
-
-void start_measurement(int16_t* error) {
-	// SCD41
-	error = scd4x_start_periodic_measurement();
-    if (error) {
-        printk("Error executing scd4x_start_periodic_measurement(): %i\n",
-               error);
-    }
-	// SVM41
-	error = svm41_start_measurement();
-    if (error) {
-        printk("Error executing svm41_start_measurement(): %i\n", error);
-    }
-}
-
-/**
-	 * example JSON file:
-	 * 
-	 * [{"sensor": SCD41, "co2": <value>, "temperature": <value>, "humidity": <value>},
-	 * {"sensor": SVM41, "humidity": <value>, "temperature": <value>, "voc": <value>}, "nox": <value>]
-	 * 
-	 * To Do: Encode data into JSON file.
-	 * 
-	*/
-
-char* data_to_json() {
-	// todo.
-	return "{\"sensor\": SCD41, \"co2\": <value>, \"temperature\": <value>, \"humidity\": <value>}, {\"sensor\": SVM41, \"humidity\": <value>, \"temperature\": <value>, \"voc\": <value>, \"nox\": <value>}";
-
-}
-
-void sensor_reading_thread(void)
-{
-	printk("sensor_reading_thread started\n");
-
+void main(void){
 	int16_t error = 0;
 
 	/* Init I2C, SC41, SVM41 */
@@ -107,76 +26,19 @@ void sensor_reading_thread(void)
 	/* Start periodic measurement */
 	start_measurement(error);
 	s_state = PERIODIC_MEASURING;
-
-	while (1) {
-		printk("->\t New Reading of Sensor Node\n");
+	
+	coap_init();
+	
+	k_msleep(5000); // for safety
+	
+	while(1) {
 		/* Read Measurement */
-		sensirion_i2c_hal_sleep_usec(1000000*2);	// 2 sec
-
-		// SCD41
-		bool data_ready_flag = false;
-		error = scd4x_get_data_ready_flag(&data_ready_flag);
-		if (error) {
-			printk("Error executing scd4x_get_data_ready_flag(): %i\n", error);
-			continue;
-		}
-		if (!data_ready_flag) {
-			printk("SCD41 Data not ready\n");
-			continue;
-		}
-		valid_SC41_data = false;
-		error = scd4x_read_measurement(&SC41_co2, &SCD41_temperature, &SCD41_humidity);
-		if (error) {
-			printk("Error executing scd4x_read_measurement(): %i\n", error);
-			continue;
-		} else if (SC41_co2 == 0) {
-			printk("Invalid SCD41 sample detected, skipping this sensor.\n");
-		} else {
-			valid_SC41_data = true;
-		}
-		// SVM41
-		valid_SVM41_data = false;
-		error = svm41_read_measured_values_as_integers(&SVM41_humidity, &SVM41_temperature,
-													&SVM41_voc_index, &SVM41_nox_index);
-		if (error) {
-			printk("Error executing svm41_read_measured_values_as_integers(): %i\n",error);
-			continue;
-		} else {
-			valid_SVM41_data = true;
-		}
-
-		/* Print Measurements */
-			printk("->\tMeasurements\n");
-			printk("  SCD41 Measurement\n");
-			if(valid_SC41_data) {
-				
-				printk("CO2: %u\n", SC41_co2);
-				printk("Temperature: %d m°C\n", SCD41_temperature);
-				printk("Humidity: %d mRH\n", SCD41_humidity);
-			}else(printk("No Valid Data.\n"));
-			
-			printk("  SVM41 Measurement\n");
-			if(valid_SVM41_data) {
-				printk("Humidity: %i milli %% RH\n", SVM41_humidity * 10);
-				printk("Temperature: %i milli °C\n", (SVM41_temperature >> 1) * 10);
-				printk("VOC index: %i (index * 10)\n", SVM41_voc_index);
-				printk("NOx index: %i (index * 10)\n", SVM41_nox_index);
-			}
-			printk("\n");
+		read_measurement();
+		print_measurement();
+		/* CoAP Message*/
+		coap_send_data_request();
+		k_msleep(5000);
 	}
-}
-
-/* Space for creating CoAP Message from Data*/
-#define TEXTBUFFER_SIZE 180
-char my_JSON[TEXTBUFFER_SIZE];
-
-/* CoAP Module */
-
-void coap_init(void){
-	otInstance * p_instance = openthread_get_default_instance();
-	otError error = otCoapStart(p_instance, OT_DEFAULT_COAP_PORT);
-	if (error!=OT_ERROR_NONE)
-		printk("Failed to start Coap: %d\n", error);
 }
 
 static void coap_send_data_response_cb(void * p_context, otMessage * p_message,
@@ -200,12 +62,31 @@ static void coap_send_data_request(void){
 	if(s_state != PERIODIC_MEASURING) {
 		if(!valid_SC41_data || !valid_SVM41_data) {
 			printk("Data not ready for transmitting via CoAP.");
+			error = -20;
 			goto exit;
 		}
 	}
 
-	const char * mySensorDataJson = data_to_json();
+	const char* my_sensor_data = create_coap_message();
+	printk("SCD41 co2: %u", SCD41_co2);
+	const char jsonBuffer[TEXTBUFFER_SIZE];
 
+    // Build the JSON string manually
+    int ret = snprintk(jsonBuffer, sizeof(jsonBuffer),
+                       "{\"SCD41\":{\"co2\":%u,\"temp\":%d,\"hum\":%d},\"SVM41\":{\"hum\":%i,\"temp\":%i,\"voc\":%i,\"nox\":%i}}",
+                       SCD41_co2, SCD41_temperature, SCD41_humidity, SVM41_humidity * 10, (SVM41_temperature >> 1) * 10, SVM41_voc_index, SVM41_nox_index);
+
+    if (ret >= 0 && ret < sizeof(jsonBuffer))
+    {
+        // Print the JSON Encoded Data to the console
+        printk("JSON Encoded Data Inside: %s\n", jsonBuffer);
+    }
+    else
+    {
+        printk("JSON Encoding Failed: %d\n", ret);
+    }
+	return jsonBuffer;
+    printk("JSON Encoded Data Outside: %s\n", my_sensor_data);
 	do{
 		myMessage = otCoapNewMessage(myInstance, NULL);
 		if (myMessage == NULL) {
@@ -224,8 +105,8 @@ static void coap_send_data_request(void){
 		error = otCoapMessageSetPayloadMarker(myMessage);
 		if (error != OT_ERROR_NONE){ break; }
 
-		error = otMessageAppend(myMessage, mySensorDataJson,
-		strlen(mySensorDataJson));
+		error = otMessageAppend(myMessage, my_sensor_data,
+		strlen(my_sensor_data));
 		if (error != OT_ERROR_NONE){ break; }
 
 		memset(&myMessageInfo, 0, sizeof(myMessageInfo));
@@ -233,6 +114,7 @@ static void coap_send_data_request(void){
 		memcpy(&myMessageInfo.mPeerAddr.mFields.m8[8], serverInterfaceID, 8);
 		myMessageInfo.mPeerPort = OT_DEFAULT_COAP_PORT;
 
+		
 		error = otCoapSendRequest(myInstance, myMessage, &myMessageInfo,
 											coap_send_data_response_cb, NULL);
 	}while(false);
@@ -246,29 +128,9 @@ static void coap_send_data_request(void){
 		}
 }
 
-
-
-void coap_thread(void)
-{
-	coap_init();
-
-	k_msleep(5000); // for safety
-
-	while(1) {	
-		coap_send_data_request();
-		k_msleep(5000);
-	}
+void coap_init(void){
+	otInstance * p_instance = openthread_get_default_instance();
+	otError error = otCoapStart(p_instance, OT_DEFAULT_COAP_PORT);
+	if (error!=OT_ERROR_NONE)
+		printk("Failed to start Coap: %d\n", error);
 }
-
-/**
- * Easy fix for sensor reading.
- * This Multi-Thread (not Openthread) starts the periodic measurement of sensor data and saves it every 2 seconds.
- * Should be adjustable so the sensors would sleep as well.
- * 
- * For now, the data might be overwritten before it is send to the server via OT.
-*/
-K_THREAD_DEFINE(sensor_reading_thread_id, THREAD0_STACKSIZE, sensor_reading_thread, NULL, NULL, NULL,
-		THREAD0_PRIORITY, 0, 5000);
-
-K_THREAD_DEFINE(coap_thread_id, THREAD1_STACKSIZE, coap_thread, NULL, NULL, NULL,
-		THREAD1_PRIORITY, 0, 10000);
--- sensorConnect/src/sensor_functionality.h
+++ sensorConnect/src/sensor_functionality.h
@@ -13,7 +13,7 @@ typedef enum{
 static sensor_state s_state = SLEEP;
 
 /* for now, data is stored like this, will be omitted later*/
-static uint16_t SC41_co2;
+static uint16_t SCD41_co2;
 static int32_t SCD41_temperature;
 static int32_t SCD41_humidity;
 
@@ -26,11 +26,12 @@ static int16_t SVM41_nox_index;
 static bool valid_SC41_data = false;
 static bool valid_SVM41_data = false;
 
-
 void clean_up_sensor_states(int16_t* error);
 
 void start_measurement(int16_t* error);
 
-void sensor_reading_thread(void);
+void read_measurement();
+
+void print_measurement();
 
 #endif /* SENSOR_FUNCTIONALITY_H */
