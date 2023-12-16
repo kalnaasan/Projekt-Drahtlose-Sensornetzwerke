84c355b1b8afa22e407ee49c70569cf2c8a803b3
Mohammed Dawoud <damohammed58@gmail.com>
2023-12-16 17:33:53 +0100

    create sensor class

---
+++ backend/src/main/java/edu/fra/uas/interiorsensors/Classes/Sensor.java
@@ -0,0 +1,33 @@
+package edu.fra.uas.interiorsensors.Classes;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode(exclude = {"valueMeasures"})
+@Entity
+@Table(name = "sensors")
+public class Sensor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "type")
+    private String type;
+
+    @ManyToOne
+    @JoinColumn(name = "room_id")
+    private Room room;
+
+    @OneToMany(mappedBy = "sensor", cascade = CascadeType.MERGE)
+    private List<ValueMeasure> valueMeasures;
+}
