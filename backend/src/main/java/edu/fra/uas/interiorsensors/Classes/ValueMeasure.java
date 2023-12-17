fd8cada43137d89c9ee86a58ef85106a04c3a1f6
Mohammed Dawoud <damohammed58@gmail.com>
2023-12-17 14:55:00 +0100

    add created_at updated_app to class valueMeasure

--- backend/src/main/java/edu/fra/uas/interiorsensors/Classes/ValueMeasure.java
+++ backend/src/main/java/edu/fra/uas/interiorsensors/Classes/ValueMeasure.java
@@ -4,9 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 import javax.persistence.*;
+import java.util.Date;
 import java.util.UUID;
 
 @Data
@@ -22,6 +25,15 @@ public class ValueMeasure {
 
     @Column
     private double value;
+
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private Date createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Date updatedAt;
 
     @ManyToOne(targetEntity = Sensor.class)
     @JoinColumn(name = "sensor_id")
