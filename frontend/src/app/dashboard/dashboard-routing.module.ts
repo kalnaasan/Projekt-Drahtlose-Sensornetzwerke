dd4dbed124af2bd2f8caf5bb6621d05770138150
Mohammed Dawoud <damohammed58@gmail.com>
2023-12-19 16:33:42 +0100

    create dashboard moudle.ts and dashboard routing.moudle.ts

--- frontend/src/app/dashboard/dashboard-routing.module.ts
+++ frontend/src/app/dashboard/dashboard-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {SensorsComponent} from "./components/sensors/sensors.component";
 import {DataViewComponent} from "./components/data-view/data-view.component";
-import {AddRoomDialogComponent} from "./components/add-room-dialog/add-room-dialog.component";
 import {AddRoomDialoggComponent} from "./components/add-room-dialogg/add-room-dialogg.component";
 import {AppComponent} from "./components/app/app.component";
 
@@ -13,7 +12,6 @@ const routes: Routes = [
     children:[
       {path:"sensors" ,component: SensorsComponent,pathMatch:'full'},
       {path:"dataView" ,component:DataViewComponent,pathMatch:'full'},
-      {path:"rooms" ,component:AddRoomDialogComponent,pathMatch :'full'},
       {path:"rooms1" ,component:AddRoomDialoggComponent,pathMatch :'full'}
     ]
   }
--- frontend/src/app/dashboard/dashboard.module.ts
+++ frontend/src/app/dashboard/dashboard.module.ts
@@ -1,33 +1,29 @@
 import { NgModule } from '@angular/core';
 import {SensorsComponent} from "./components/sensors/sensors.component";
 import {MatCardModule} from "@angular/material/card";
-import {ReactiveFormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {MatDialogModule} from "@angular/material/dialog";
 import {MatInputModule} from "@angular/material/input";
 import {DashboardRoutingModule} from "./dashboard-routing.module";
 import { AppComponent } from './components/app/app.component';
 import { DataViewComponent } from './components/data-view/data-view.component';
-import { AddRoomDialogComponent } from './components/add-room-dialog/add-room-dialog.component';
 import {MatButtonModule} from "@angular/material/button";
 import {MatSelectModule} from "@angular/material/select";
-import { MatOptionModule } from '@angular/material/core';
+import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
 import { AddRoomDialoggComponent } from './components/add-room-dialogg/add-room-dialogg.component';
+import {NgForOf, NgIf} from "@angular/common";
 
 
 
 @NgModule({
   declarations: [
-AddRoomDialogComponent,
     SensorsComponent,
     AppComponent,
     DataViewComponent,
     AddRoomDialoggComponent,
 
-
-
   ],
   exports: [
-    AddRoomDialogComponent,
     SensorsComponent,
     DataViewComponent
   ],
@@ -38,8 +34,11 @@ AddRoomDialogComponent,
     MatDialogModule,
     MatInputModule,
     MatButtonModule,
+    NgMultiSelectDropDownModule.forRoot(),
     MatSelectModule,
-    MatOptionModule,
+    FormsModule,
+    NgIf,
+    NgForOf,
 
   ]
 })
