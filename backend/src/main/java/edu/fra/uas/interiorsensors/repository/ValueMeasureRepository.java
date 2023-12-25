1b2220978e8588db030b12d71498c07659a6ffb0
Kaddour Alnaasan <kaddour.alnaasan@bellavista-film.com>
2023-12-25 21:25:33 +0100

    create findAllBySensorName

--- backend/src/main/java/edu/fra/uas/interiorsensors/repository/ValueMeasureRepository.java
+++ backend/src/main/java/edu/fra/uas/interiorsensors/repository/ValueMeasureRepository.java
@@ -4,9 +4,13 @@ import edu.fra.uas.interiorsensors.model.ValueMeasure;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface ValueMeasureRepository extends JpaRepository<ValueMeasure, UUID> {
     boolean existsByValue(double value);
+
+    List<ValueMeasure> findAllBySensor_NameAndReadAtBetweenOrderByCreatedAtAsc(String sensor_name, LocalDateTime start, LocalDateTime end);
 }
