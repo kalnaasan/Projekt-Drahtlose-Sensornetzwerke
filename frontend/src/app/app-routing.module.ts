e90df462f8d72ddde1ba694c6ad7349b2f34fd71
Kaddour Alnaasan <kaddour.alnaasan@bellavista-film.com>
2023-12-25 22:31:44 +0100

    use loadChildren

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,14 +1,8 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import {DataViewComponent} from "./shared/components/data-view/data-view.component";
-import {SensorsComponent} from "./shared/components/sensors/sensors.component";
-import {AddRoomDialogComponent} from "./shared/components/add-room-dialog/add-room-dialog.component";
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
 const routes: Routes = [
-  { path: 'dataView', component: DataViewComponent, pathMatch: 'full' },
-  { path: 'sensors', component: SensorsComponent, pathMatch: 'full' },
-  {path: 'rooms', component: AddRoomDialogComponent, pathMatch: 'full'},
-  { path: '', redirectTo: '/dataView', pathMatch: 'full' },
+  {path: '', loadChildren: () => import(`./dashboard/dashboard.module`).then((s) => s.DashboardModule)}
 ];
 
 
@@ -16,4 +10,5 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
