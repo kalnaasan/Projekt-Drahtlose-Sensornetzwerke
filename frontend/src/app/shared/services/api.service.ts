24ce3e428fd3ae008ec1936fb5fd5451aa682562
Kaddour Alnaasan <kaddour.alnaasan@bellavista-film.com>
2024-01-08 15:54:47 +0100

    use apiURL from environment

--- frontend/src/app/shared/services/api.service.ts
+++ frontend/src/app/shared/services/api.service.ts
@@ -1,19 +1,22 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
+import {environment} from "../../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
+  private apiUrl = environment.SERVER_URL + '/api/v1/web';
+
   constructor(private http: HttpClient) {
   }
 
   public getValueBySensorTypeAndDate(sensorName: string, from: string, to: string): Observable<any> {
-    return this.http.get<any>(`http://localhost:8080/api/v1/web/values?sensor-name=${sensorName}&from=${from}&to=${to}`);
+    return this.http.get<any>(`${this.apiUrl}/values?sensor-name=${sensorName}&from=${from}&to=${to}`);
   }
   public getSensorsByRoom(roomId: string): Observable<any>{
-    return this.http.get<any>(`http://localhost:8080/api/v1/web/sensors?room-id=${roomId}`);
+    return this.http.get<any>(`${this.apiUrl}/sensors?room-id=${roomId}`);
   }
 }
