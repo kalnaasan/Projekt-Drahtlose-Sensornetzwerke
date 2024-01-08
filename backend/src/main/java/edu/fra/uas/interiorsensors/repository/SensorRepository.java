09f96dcb7869f96648c187e438fe3baaa62a46a5
Kaddour Alnaasan <kaddour.alnaasan@bellavista-film.com>
2024-01-08 10:52:07 +0100

    get Sensor by room-id

--- backend/src/main/java/edu/fra/uas/interiorsensors/repository/SensorRepository.java
+++ backend/src/main/java/edu/fra/uas/interiorsensors/repository/SensorRepository.java
@@ -4,12 +4,15 @@ import edu.fra.uas.interiorsensors.model.Sensor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface SensorRepository extends JpaRepository<Sensor, UUID> {
     Optional<Sensor> findByName(String name);
+
+    List<Sensor> findAllByRoom_Id(UUID roomId);
 
     boolean existsByName(String name);
 }
