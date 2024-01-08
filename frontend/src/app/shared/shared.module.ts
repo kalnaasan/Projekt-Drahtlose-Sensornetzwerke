06ca74e0a9cbcec8019997941911e37cc07bef1e
Kaddour Alnaasan <kaddour.alnaasan@bellavista-film.com>
2024-01-08 10:37:22 +0100

    import Chart-Library

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -1,21 +1,21 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { SharedRoutingModule } from './shared-routing.module';
-import { NavbarComponent } from './components/navbar/navbar.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {NavbarComponent} from './components/navbar/navbar.component';
 import {MatIconModule} from "@angular/material/icon";
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatButtonModule} from "@angular/material/button";
 import {MatSidenavModule} from "@angular/material/sidenav";
-import { DataViewComponent } from './components/data-view/data-view.component';
+import {DataViewComponent} from './components/data-view/data-view.component';
 import {MatCardModule} from "@angular/material/card";
 import {SidebarComponent} from "./components/sidebar/sidebar.component";
-import { SensorsComponent } from './components/sensors/sensors.component';
-import { RouterModule } from '@angular/router';
-import { AddRoomDialogComponent } from './components/add-room-dialog/add-room-dialog.component';
+import {SensorsComponent} from './components/sensors/sensors.component';
+import {RouterModule} from '@angular/router';
+import {AddRoomDialogComponent} from './components/add-room-dialog/add-room-dialog.component';
 import {MatDialogModule} from "@angular/material/dialog";
 import {ReactiveFormsModule} from "@angular/forms";
 import {MatInputModule} from "@angular/material/input";
-
+import {NgApexchartsModule} from 'ng-apexcharts';
+import {ChartComponent} from './components/chart/chart.component';
 
 
 @NgModule({
@@ -25,20 +25,18 @@ import {MatInputModule} from "@angular/material/input";
     DataViewComponent,
     SensorsComponent,
     AddRoomDialogComponent,
-
-
+    ChartComponent,
   ],
-  exports: [
-    NavbarComponent,
-    SidebarComponent,
-    DataViewComponent,
-    SensorsComponent,
-    AddRoomDialogComponent,
-
-  ],
+    exports: [
+        NavbarComponent,
+        SidebarComponent,
+        DataViewComponent,
+        SensorsComponent,
+        AddRoomDialogComponent,
+        ChartComponent
+    ],
   imports: [
     CommonModule,
-    SharedRoutingModule,
     MatIconModule,
     MatToolbarModule,
     MatButtonModule,
@@ -47,7 +45,9 @@ import {MatInputModule} from "@angular/material/input";
     RouterModule,
     MatDialogModule,
     ReactiveFormsModule,
-    MatInputModule
+    MatInputModule,
+    NgApexchartsModule,
   ]
 })
-export class SharedModule { }
+export class SharedModule {
+}
