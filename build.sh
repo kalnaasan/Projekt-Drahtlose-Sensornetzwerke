a4cd68dd48b6779bb923628ab468898879776669
Admin <admin@luftqualitaet-ws23.informatik.fb2.frankfurt-university.de>
2024-01-14 00:38:23 +0000

    fix deployment script

--- build.sh
+++ build.sh
@@ -1,10 +1,33 @@
 #! /bin/bash
 
-docker compose -p sensor down
+# Name des Netzwerks
+network_name="compose"
+
+#  pruefen, ob das Netzwerk bereits vorhanden ist
+if docker network inspect "$network_name" &>/dev/null; then
+    echo "Das Netzwerk '$network_name' existiert bereits."
+else
+    # Das Netzwerk existiert nicht, daher erstellen
+    docker network create --subnet 10.20.30.0/24 "$network_name"
+    echo "Das Netzwerk '$network_name' wurde erfolgreich erstellt."
+fi
 
 # Change to the DB folder and start DB for deployment
 cd ./DB/
-docker compose -p temp up -d
+docker compose -p db up -d
+
+# Funktion zur ueberpruefen, ob MySQL bereit ist
+check_mysql() {
+  docker exec mysql mysqladmin ping -hmysql -u root --password=mysql &> /dev/null
+}
+
+# Warten, bis MySQL bereit ist
+while ! check_mysql; do
+  echo "MySQL ist nicht bereit. Warte 5 Sekunden..."
+  sleep 5
+done
+
+echo "MySQL ist bereit!"
 
 # Change to the CoAPServer folder and build the Docker image
 cd ../CoAPServer/
@@ -22,24 +45,8 @@ docker build -t sensor-api:latest --progress=plain .
 cd ../frontend
 docker build -t sensor-nginx:latest --progress=plain .
 
-# Change to the DB folder and stop DB for deployment
-cd ../DB/
-docker compose -p temp down
-
-# Name des Netzwerks
-network_name="configurator_compose"
-
-#  pruefen, ob das Netzwerk bereits vorhanden ist
-if docker network inspect "$network_name" &>/dev/null; then
-    echo "Das Netzwerk '$network_name' existiert bereits."
-else
-    # Das Netzwerk existiert nicht, daher erstellen
-    docker network create --subnet 10.20.30.0/24 "$network_name"
-    echo "Das Netzwerk '$network_name' wurde erfolgreich erstellt."
-fi
-
 # Change to the main folder and start application
-cd ../
+cd ..
 
 # start stack
 docker compose -p sensor up -d
