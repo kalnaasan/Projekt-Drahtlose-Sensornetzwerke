49b0216b834311fd6cd9e11faca0eea3671d8b6f
Kaddour Alnaasan <kaddour.alnaasan@bellavista-film.com>
2024-01-25 11:13:14 +0100

    get status from chart and show it in dialog

--- frontend/src/app/dashboard/components/room/room.component.ts
+++ frontend/src/app/dashboard/components/room/room.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {ApiService} from "../../../shared/services/api.service";
 import {Sensor} from "../../../shared/model/sensor";
+import {MatDialog} from "@angular/material/dialog";
+import {StatusDialogComponent} from "../status-dialog/status-dialog.component";
 
 @Component({
   selector: 'app-room',
@@ -11,14 +13,36 @@ import {Sensor} from "../../../shared/model/sensor";
 export class RoomComponent {
 
   public sensors: Sensor[] = [];
+  public status: any[] = []
+  public resolveStatus: boolean = false;
 
   constructor(private route: ActivatedRoute,
-              private apiService: ApiService) {
+              private apiService: ApiService,
+              public dialog: MatDialog) {
     this.route.params.subscribe(params => {
       this.apiService.getSensorsByRoom(params['id']).subscribe({
         next: (res: any) => this.sensors = res.data,
         error: (err: any) => console.log(err)
       })
+    });
+  }
+
+  addMessage(newItem: any) {
+    this.status.push(newItem);
+    if (this.status.length === this.sensors.length) {
+      console.log(this.status);
+      this.openDialog();
+    }
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(StatusDialogComponent, {
+      data: this.status,
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      this.status = [];
+      console.log('The dialog was closed', this.status);
     });
   }
 }
