6f579e5519d4cb29ae2d2cf474de071d6e9ae1c4
Kaddour Alnaasan <kaddour.alnaasan@bellavista-film.com>
2024-01-27 22:03:55 +0100

    create get by day and week

--- backend/src/main/java/edu/fra/uas/interiorsensors/repository/ValueMeasureRepository.java
+++ backend/src/main/java/edu/fra/uas/interiorsensors/repository/ValueMeasureRepository.java
@@ -2,6 +2,7 @@ package edu.fra.uas.interiorsensors.repository;
 
 import edu.fra.uas.interiorsensors.model.ValueMeasure;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
@@ -13,4 +14,10 @@ public interface ValueMeasureRepository extends JpaRepository<ValueMeasure, UUID
     boolean existsByValue(double value);
 
     List<ValueMeasure> findAllBySensor_NameAndReadAtBetweenOrderByReadAtAsc(String sensor_name, LocalDateTime start, LocalDateTime end);
+
+    @Query("SELECT HOUR(vm.readAt) AS readAt, AVG(vm.value) AS value FROM ValueMeasure vm LEFT JOIN Sensor s ON s.id = vm.sensor.id Where s.name=:sensorName AND vm.readAt>=:start AND vm.readAt<=:end GROUP BY HOUR(vm.readAt) ORDER BY HOUR (vm.readAt)")
+    List<Object[]> findAllBySensor_NameAndReadAtBetweenAndAverageReadAtOrderByReadAt(String sensorName, LocalDateTime start, LocalDateTime end);
+
+    @Query(value = "SELECT HOUR(vm.read_at) DIV 4 as period, AVG(vm.value) as averageValue FROM value_measure vm JOIN sensors s WHERE s.name = ?1 AND vm.read_at BETWEEN ?2 AND ?3 GROUP BY period ORDER BY period", nativeQuery = true)
+    List<Object[]> findAllBySensor_NameAndReadAtBetweenAndAverageByFourHours_ReadAtOrderByReadAt(String sensorName, LocalDateTime from, LocalDateTime to);
 }
