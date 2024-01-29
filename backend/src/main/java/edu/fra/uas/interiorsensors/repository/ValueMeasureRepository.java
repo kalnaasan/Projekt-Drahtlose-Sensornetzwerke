82740c498cd793e630abbc6f925fe946f84cdd37
Mohammed Dawoud <damohammed58@gmail.com>
2024-01-29 13:06:23 +0100

    create new List in ValueMeasureRepository

--- backend/src/main/java/edu/fra/uas/interiorsensors/repository/ValueMeasureRepository.java
+++ backend/src/main/java/edu/fra/uas/interiorsensors/repository/ValueMeasureRepository.java
@@ -1,6 +1,7 @@
 package edu.fra.uas.interiorsensors.repository;
 
 import edu.fra.uas.interiorsensors.model.ValueMeasure;
+import org.springframework.data.domain.Limit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,6 +12,6 @@ import java.util.UUID;
 @Repository
 public interface ValueMeasureRepository extends JpaRepository<ValueMeasure, UUID> {
     boolean existsByValue(double value);
-
+    List<ValueMeasure> findAllBySensor_IdOrderByReadAtDesc(UUID sensorId, Limit limit);
     List<ValueMeasure> findAllBySensor_NameAndReadAtBetweenOrderByReadAtAsc(String sensor_name, LocalDateTime start, LocalDateTime end);
 }
