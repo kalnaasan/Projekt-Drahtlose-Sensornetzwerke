015d7066b812d4868c951e54e5aaf942c94b914a
Kaddour Alnaasan <kaddour.alnaasan@bellavista-film.com>
2024-01-29 13:46:21 +0100

    create shape-service in FE

---
+++ frontend/src/app/shared/services/shape.service.ts
@@ -0,0 +1,23 @@
+import {Injectable} from '@angular/core';
+import {environment} from "../../../environments/environment";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Shape} from "../model/shape";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ShapeService {
+  private apiUrl = environment.SERVER_URL + '/api/v1/web/shapes';
+
+  constructor(private http: HttpClient) {
+  }
+
+  public getShapes(): Observable<any> {
+    return this.http.get<any>(this.apiUrl);
+  }
+
+  public createShape(shape: Shape): Observable<any> {
+    return this.http.post<any>(this.apiUrl, shape);
+  }
+}
